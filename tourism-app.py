0a738b59682be28694a6c3f31d3a25b6da6c57a2
Rifai Martin <academyyyy12@gmail.com>
2023-12-16 05:38:02 +0700

    update

--- tourism-app.py
+++ tourism-app.py
@@ -38,61 +38,32 @@ if uploaded_file is not None:
 #         return features
 #     input_df = user_input_features()
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-tourism_raw = pd.read_csv('tourism_rating_cleaned.csv')
-tourism = tourism_raw.drop(columns=['Age'])
-tourism = tourism_raw.drop(columns=['Category'])
-tourism = tourism_raw.drop(columns=['City'])
-tourism = tourism_raw.drop(columns=['Coordinate'])
-tourism = tourism_raw.drop(columns=['Description'])
-
-# df = tourism_raw 
-df = tourism
-
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-# encode = ['sex','island']
-# for col in encode:
-#     dummy = pd.get_dummies(df[col], prefix=col)
-#     df = pd.concat([df,dummy], axis=1)
-#     del df[col]
-# df = df[:1] # Selects only the first row (the user input data)
-
-# Displays the user input features
-st.subheader('User Input features')
-
-if uploaded_file is not None:
-    st.write(df)
-# else:
-#     st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-#     st.write(df)
-
-# Memuat model yang benar (misalnya RandomForestClassifier)
+# Load trained model
 load_clf = pickle.load(open('tourism_rating.pkl', 'rb'))
 
-# Mengecek kembali fitur-fitur yang akan digunakan untuk prediksi
-features = ['User_Id', 'Location', 'Place_Id', 'Time_Minutes']  # Sesuaikan dengan fitur yang digunakan saat melatih model
+# Load data
+data = pd.read_csv('tourism_rating_cleaned.csv')
 
-# Memastikan dataframe yang digunakan sesuai dengan fitur yang diharapkan
-df = pd.read_csv('tourism_rating_cleaned.csv')
-df = df[features]  # Mengambil hanya fitur-fitur yang digunakan saat pelatihan
+# Display uploaded data
+st.subheader('Uploaded Data')
+st.write(data)
 
-# Menampilkan fitur-fitur yang akan digunakan untuk prediksi
-st.subheader('User Input features')
-st.write(df)
+# Prepare features for prediction
+features = ['User_Id', 'Location',  'Place_Id', 'Time_Minutes']  # Sesuaikan dengan fitur yang ingin digunakan # Adjust features accordingly
 
-categorical_columns = ['Location', 'Place_Name', 'Description', 'Category', 'City']
-
-# Lakukan One-Hot Encoding pada kolom-kolom kategorikal
+# Process categorical columns using One-Hot Encoding
+categorical_columns = ['Location', 'Place_Id']  # Categorical columns that need encoding
 for col in categorical_columns:
-    encoded_cols = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df, encoded_cols], axis=1)
-    df.drop(col, axis=1, inplace=True)
+    encoded_cols = pd.get_dummies(data[col], prefix=col)
+    data = pd.concat([data, encoded_cols], axis=1)
+    data.drop(col, axis=1, inplace=True)
 
-# Melakukan prediksi dengan model yang telah dimuat
-prediction = load_clf.predict(df)
+# Prepare data for prediction (select only relevant features)
+data_for_prediction = data[features]
 
-# Menampilkan hasil prediksi
+# Perform prediction using the loaded model
+prediction = load_clf.predict(data_for_prediction)
+
+# Display prediction
 st.subheader('Prediction')
 st.write(prediction)
