72c86265377bdce81736880265112f1f1688e557
Rifai Martin <academyyyy12@gmail.com>
2023-12-16 05:13:52 +0700

    update

--- tourism-app.py
+++ tourism-app.py
@@ -22,29 +22,27 @@ st.sidebar.markdown("""
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
-# else:
-#     def user_input_features():
-#         island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-#         sex = st.sidebar.selectbox('Sex',('male','female'))
-#         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-#         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-#         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-#         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-#         data = {'island': island,
-#                 'bill_length_mm': bill_length_mm,
-#                 'bill_depth_mm': bill_depth_mm,
-#                 'flipper_length_mm': flipper_length_mm,
-#                 'body_mass_g': body_mass_g,
-#                 'sex': sex}
-#         features = pd.DataFrame(data, index=[0])
-#         return features
-    # input_df = user_input_features()
+else:
+    def user_input_features():
+        location = st.sidebar.selectbox('Location',('Semarang','Jawa Tengah'))
+        place_name = st.sidebar.selectbox('Place Name',('Candi Ratu Boko'))
+        description = st.sidebar.slider('Description', (''))
+        category = st.sidebar.slider('Category', ('Budaya','Bahari'))
+        city = st.sidebar.slider('City', ('Jakarta','Semarang'))
+        data = {'location': location,
+                'place_name': place_name,
+                'description': description,
+                'category': category,
+                'city': city}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
 
 # Combines user input features with entire penguins dataset
 # This will be useful for the encoding phase
-df = pd.read_csv('tourism_rating_cleaned.csv')
-# penguins = tourism_raw.drop(columns=['species'])
-# df = pd.concat([input_df,penguins],axis=0)
+tourism_raw = pd.read_csv('tourism_rating_cleaned.csv')
+tourism = tourism_raw.drop(columns=['Unnamed: 11'])
+df = pd.concat([input_df,tourism],axis=0)
 
 # Encoding of ordinal features
 # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
