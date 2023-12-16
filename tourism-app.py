522491ef03e813e2431818dbff0bf0d35e4acf6d
Rifai Martin <academyyyy12@gmail.com>
2023-12-16 10:50:25 +0700

    update

--- tourism-app.py
+++ tourism-app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+
+# Load trained model
+load_clf = pickle.load(open('tourism_rating.pkl', 'rb'))
 
 st.write("""
 # Tourism Rating Predict App
 
 This app predicts the **Rating for place**!
-
-Data obtained from kaagle [data tourism](https://www.kaggle.com/datasets/aprabowo/indonesia-tourism-destination) .
 """)
 
 st.sidebar.header('User Input Features')
@@ -22,65 +22,35 @@ st.sidebar.markdown("""
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
-# else:
-#     def user_input_features():
-#         location = st.sidebar.selectbox('Location',('Semarang','Jawa Tengah'))
-#         place_name = st.sidebar.selectbox('Place Name',('Candi Ratu Boko'))
-#         description = st.sidebar.slider('Description', (''))
-#         category = st.sidebar.slider('Category', ('Budaya','Bahari'))
-#         city = st.sidebar.slider('City', ('Jakarta','Semarang'))
-#         data = {'location': location,
-#                 'place_name': place_name,
-#                 'description': description,
-#                 'category': category,
-#                 'city': city}
-#         features = pd.DataFrame(data, index=[0])
-#         return features
-    # input_df = user_input_features()
-
-# Load trained model
-load_clf = pickle.load(open('tourism_rating.pkl', 'rb'))
 
 # Load data
 data = pd.read_csv('tourism_rating_cleaned.csv')
-
 data.fillna(value="0", inplace=True)
 
-# Load data
+# Load additional data
 data2 = pd.read_csv('sumatera.csv')
-
 data2.fillna(value="0", inplace=True)
-
 
 # Display uploaded data
 st.subheader('Uploaded Data')
 st.write(data)
 
-# tujuan dari pemodelan adalah untuk melakukan prediksi nilai 'Place_Ratings' yang 
-# merupakan variabel numerik atau kontinu. Linear Regression sering digunakan 
-# untuk memodelkan hubungan linier antara variabel independen (fitur) dan variabel 
-# dependen (target) dengan asumsi hubungan tersebut dapat dijelaskan melalui suatu garis lurus.
+features = ['User_Id', 'Place_Id', 'Time_Minutes', 'Price']  # Sesuaikan dengan fitur yang ingin digunakan
 
-
-features = ['User_Id', 'Place_Id', 'Time_Minutes','Price']  # Sesuaikan dengan fitur yang ingin digunakan # Adjust features accordingly
-# Prepare data for prediction (select only relevant features)
+# Prepare data for prediction
 data_for_prediction = data[features]
-# .iloc[[0]]
-
-# Perform prediction using the loaded model
 prediction = load_clf.predict(data_for_prediction)
 
-
 data_for_prediction2 = data2[features]
-# .iloc[[0]]
-
-# Perform prediction using the loaded model
 prediction2 = load_clf.predict(data_for_prediction2)
 
-# Display prediction
-st.subheader('Prediction Jawa Barat')
-st.write(prediction)
+# Sort predictions to find the highest values
+sorted_indices = np.argsort(prediction)[::-1]
+sorted_indices2 = np.argsort(prediction2)[::-1]
 
 # Display prediction
-st.subheader('Prediction Sumatra')
-st.write(prediction2)
+st.subheader('Top Predictions Jawa Barat')
+st.write(prediction[sorted_indices[:10]])
+
+st.subheader('Top Predictions Sumatra')
+st.write(prediction2[sorted_indices2[:10]])
